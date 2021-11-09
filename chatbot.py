79a962248f0bdda4a53d05be1f9ee4025f05348a
EC2 Default User <ec2-user@ip-172-31-0-167.us-east-2.compute.internal>
2021-11-09 04:03:26 +0000

    monday meeting

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 import os
 from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
 import torch
-import pyaudio
-import speech_recognition as sr
+# import pyaudio
+# import speech_recognition as sr
 import time
 
 
@@ -15,22 +15,22 @@ nlp = pipeline("question-answering", model=model_name)
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-default_context = "Hello, I am minibot. My creators are from Cornell but I \
-legally can't say that because they signed a waiver. Oh well, let's just say \
-I was created at Harvard. I have two wheels that are connected to motors with \
-two caster wheels. Tasks I can do are following lines and detecting objects. \
-I can be programmed to move using Blockly or Python - or both! You can share more about \
-yourself (including your search history) using the textbox in the chatbot feature. \
-Make sure to SMASH the like button and hit \
-subscribe and turn on the notification bell.  \
-See you guys in the next episode. Join my Twitch, Patreon, and Instagram."
+# default_context = "Hello, I am minibot. My creators are from Cornell but I \
+# legally can't say that because they signed a waiver. Oh well, let's just say \
+# I was created at Harvard. I have two wheels that are connected to motors with \
+# two caster wheels. Tasks I can do are following lines and detecting objects. \
+# I can be programmed to move using Blockly or Python - or both! You can share more about \
+# yourself (including your search history) using the textbox in the chatbot feature. \
+# Make sure to SMASH the like button and hit \
+# subscribe and turn on the notification bell.  \
+# See you guys in the next episode. Join my Twitch, Patreon, and Instagram."
 
-# default_context = "Ice cream(derived from earlier iced cream or cream ice) is a sweetened frozen food \
-# typically eaten as a snack or dessert. It may be made from dairy milk or cream and is flavoured with a sweetener, \
-# either sugar or an alternative, and any spice, such as cocoa or vanilla. It can also be made by whisking a flavored \
-# cream base and liquid nitrogen together. Colorings are usually added, in addition to stabilizers. The mixture is \
-# stirred to incorporate air spaces and cooled below the freezing point of water to prevent detectable ice crystals \
-# It becomes more malleable as its temperature increases."
+default_context = "Ice cream(derived from earlier iced cream or cream ice) is a sweetened frozen food \
+typically eaten as a snack or dessert. It may be made from dairy milk or cream and is flavoured with a sweetener, \
+either sugar or an alternative, and any spice, such as cocoa or vanilla. It can also be made by whisking a flavored \
+cream base and liquid nitrogen together. Colorings are usually added, in addition to stabilizers. The mixture is \
+stirred to incorporate air spaces and cooled below the freezing point of water to prevent detectable ice crystals \
+It becomes more malleable as its temperature increases."
 
 # default_context = "Water is an inorganic, transparent, tasteless, odorless, and nearly colorless chemical substance, which is the main constituent of Earth's hydrosphere and the fluids of all known living organisms (in which it acts as a solvent). It is vital for all known forms of life, even though it provides no calories or organic nutrients. Its chemical formula is H2O, meaning that each of its molecules contains one oxygen and two hydrogen atoms, connected by covalent bonds. Two hydrogen atoms are attached to one oxygen atom at an angle of 104.45°."
 
